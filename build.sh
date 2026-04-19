6f075572f1f764eacacf2d2a756889b71824100b
Vinny Seeram <vinny@Vinnys-Air.lan>
2026-04-19 19:56:06 -0400

    build script build dir restore

--- build.sh
+++ build.sh
@@ -11,7 +11,12 @@ main() {
     echo "📦 Installing Hugo v${HUGO_VERSION}..."
     curl -LJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
     tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+    cp hugo /opt/buildhome/
     rm LICENSE README.md "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+
+    # Set PATH
+    echo "🔧 Configuring environment..."
+    export PATH="/opt/buildhome:$PATH"
 
     # Initialize submodules (for themes)
     echo "🎨 Setting up blowfish..."
